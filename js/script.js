a4ef362179101c357de7dc326c742daa60640cd5
Ali-vielseitig <moradi.brat@gmail.com>
2021-11-20 16:47:19 +0100

    tis is my first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+// function html_page(){
+//     var html = document.getElementById("items");
+//     if (html.style.display === "none"){
+//         html.style.display = "block";
+//     }else{
+//         html.style.display = "none";
+//     }
+// }
